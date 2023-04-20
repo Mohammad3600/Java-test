9bc6d82e2badcb02c301550995eb6a84cd6d2d5c
Vempalli Mohammad <Vempalli.Mohammad@Triconinfotech.com>
2023-04-20 23:28:13 +0530

    intial commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String ...a){
+        System.out.println(add2Num(1,2));
+    }
+    public static int add2Num(int a, int b){
+        return a+b;
+    }
+}
